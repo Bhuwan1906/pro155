854b341bb91ee69676fb27bb362c3eacc7756314
Bhuwan1906 <techbhuwan1906@gmail.com>
2023-01-09 15:44:37 +0530

    first commit

---
+++ PRO-C155-Student-Activity-main/components/Game.js
@@ -0,0 +1,20 @@
+AFRAME.registercomponent("game-play",{
+    schema:{
+        elementId: {type: "string",default:"#ring1"},
+
+    },
+    update: function(){
+        this.isCollided(this.data.elementId);
+
+    },
+    isCollided: function(elementId){
+        const element = document.quarySelector(elementId);
+        element.addEventListener("collide",(e)=>{
+            if(elementId.includes("#ring")){
+            console.log(elementId+"collision");
+            } else if (elementId.includes("#hurdle")){
+                console.log("bird collision")
+            }
+        })
+    }
+})
